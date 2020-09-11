7c159c31c53680811889baa41d3af9c6c87d69f6
Christopher Barrett <municipalshock@gmail.com>
2020-09-10 18:31:02 -0700

    Comment section

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  console.log(props.Posts);
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map((element) => {
+        return <Post post={element} key={element.id} likePost={likePost} />;
+      })}
     </div>
   );
 };
